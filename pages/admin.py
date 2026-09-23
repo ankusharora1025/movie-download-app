c9731abbb5f8abdccb323c3df3e947ca0838fdb8
ankusharora1025 <ankusharora1025@gmail.com>
2026-09-23 11:57:57 +0400

    Update admin.py

--- pages/admin.py
+++ pages/admin.py
@@ -10,7 +10,7 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="collapsed"
 )
-st.success("TEST: CINEMAHUB NEW APP.PY IS RUNNING")
+
 # =====================================================
 # SUPABASE
 # =====================================================
@@ -603,7 +603,3 @@ st.markdown("---")
 st.caption(
     "© 2026 CinemaHub • Movies • Series • Entertainment"
 )
-
-# CinemaHub deployment refresh
-
-# CinemaHub v2
