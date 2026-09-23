d2f7bd0bedc3d83d5bb7b24cd6f96d4b36fa6e59
ankusharora1025 <ankusharora1025@gmail.com>
2026-09-23 11:22:55 +0400

    Update admin.py

--- pages/admin.py
+++ pages/admin.py
@@ -10,7 +10,7 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="collapsed"
 )
-
+st.success("TEST: CINEMAHUB NEW APP.PY IS RUNNING")
 # =====================================================
 # SUPABASE
 # =====================================================
