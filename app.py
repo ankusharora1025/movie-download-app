91db42cba731b77be4a73288f20ebe9b758b66c1
ankusharora1025 <ankusharora1025@gmail.com>
2026-09-23 16:06:07 +0400

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ st.markdown(
 
     .movie-poster {
         width: 100%;
-        height: 330px;
+        height: 260px;
         overflow: hidden;
         border-radius: 14px;
         background: #171a21;
@@ -69,7 +69,7 @@ st.markdown(
 
     .no-poster {
         width: 100%;
-        height: 330px;
+        height: 260px;
         border-radius: 14px;
         background: #19324a;
         display: flex;
